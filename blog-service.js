874dd7dc27217967011e5ecc0c3764d93744353d
samrath singh <ssandhu75@myseneca.ca>
2024-08-30 10:46:54 -0400

    last commit

--- blog-service.js
+++ blog-service.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 //const { getEnabledCategories } = require("trace_events");
 let posts = [];
 let categories = [];
+let PostsByCategory = [];
+let PostsByMinDate = [];
+let PostsById = [];
 function initialize() {
   return new Promise((resolve, reject) => {
     fs.readFile("data/posts.json", "utf8", (err, data) => {
@@ -50,9 +53,56 @@ function getCategories() {
   });
 }
 
+function addPost(postData) {
+  return new Promise((resolve, reject) => {
+    if (postData.published === undefined) {
+      postData.published = false;
+    }
+    postData.id = posts.length + 1;
+    posts.push(postData);
+    resolve(postData);
+  });
+}
+
+function getPostsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    PostsByCategory = posts.filter((post) => post.category === category);
+    if (PostsByCategory.length > 0) {
+      resolve(PostsByCategory);
+    } else reject("no results found ");
+  });
+}
+
+function getPostsByMinDate(minDateStr) {
+  return new Promise((resolve, reject) => {
+    PostsByMinDate = posts.filter(
+      (post) => new Date(post.postDate) >= new Date(minDateStr)
+    );
+    if (PostsByMinDate.length > 0) {
+      resolve(PostsByMinDate);
+    } else {
+      reject("no results found");
+    }
+  });
+}
+
+function getPostById(id) {
+  return new Promise((resolve, reject) => {
+    PostsById = posts.filter((post) => post.id === id);
+    if (PostsById.length > 0) {
+      resolve(PostsById[0]);
+    } else {
+      reject("no results found");
+    }
+  });
+}
 module.exports = {
   initialize,
   getAllPosts,
   getPublishedPosts,
   getCategories,
+  addPost,
+  getPostsByCategory,
+  getPostsByMinDate,
+  getPostById,
 };
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
 const posts = require("./data/posts.json");
 const categories = require("./data/categories.json");
 const blogService = require("./blog-service");
 const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8001;
+
+cloudinary.config({
+  cloud_name: "dyxegadrc",
+  api_key: "638954724841219",
+  api_secret: "ovdWTvISmMmtMi-yqVg7unekDx8",
+  secure: true,
+});
+
+const upload = multer();
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -34,14 +47,84 @@ app.get("/blog", (req, res) => {
 });
 
 app.get("/posts", (req, res) => {
+  if (req.query.category) {
+    const category = parseInt(req.query.category, 10);
+    if (category < 1 || category > 5) {
+      return res.status(400).json({ message: "Invalid category filter" });
+    }
+
+    blogService.getPostsByCategory(category).then((PostsByCategory) => {
+      res.json(PostsByCategory);
+    });
+  } else if (req.query.minDate) {
+    const minDate = new Date(req.query.minDate);
+    blogService
+      .getPostsByMinDate(minDate)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  } else {
+    blogService
+      .getAllPosts()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
+  }
+});
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/addPost.html"));
+});
+app.get("/post/:value", (req, res) => {
+  const value = parseInt(req.params.value, 10);
   blogService
-    .getAllPosts()
+    .getPostById(value)
     .then((data) => {
       res.json(data);
     })
     .catch((err) => {
       res.status(500).send(err);
     });
+});
+
+app.post("/posts/add", upload.single("featureImage"), async (req, res) => {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
+
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
+
+  async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+    return result;
+  }
+
+  upload(req).then((uploaded) => {
+    req.body.featureImage = uploaded.url;
+
+    blogService
+      .addPost(req.body)
+      .then(() => {
+        res.redirect("/posts");
+      })
+      .catch((err) => {
+        console.log(err + "error in adding post");
+      });
+  });
 });
 app.get("/categories", (req, res) => {
   blogService
